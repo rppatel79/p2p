7d95dd735a23a856729f31a31cc55de1b4d729db
rppatel79 <ritesh.pri.patel@gmail.com>
2016-01-10 10:06:33 -0500

    Added rate restriction

--- lendingClub/src/main/java/com/rp/p2p/loan_selector/FilteredLoanSelector.java
+++ lendingClub/src/main/java/com/rp/p2p/loan_selector/FilteredLoanSelector.java
@@ -110,6 +110,11 @@ public class FilteredLoanSelector implements LoansSelector
                 logger_.info("Failed VALID_GRADE [" + loan.getGrade()+"] " + loan.getId());
                 brokenRules++;
             }
+            if((loan.getIntRate() < 0.08))
+            {
+                logger_.info("Failed INT_RATE [" + loan.getIntRate()+"] " + loan.getId());
+                brokenRules++;
+            }
             else
             {
                 if (allInvestedLoans.contains(loan.getId()))
