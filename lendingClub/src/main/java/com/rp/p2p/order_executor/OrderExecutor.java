ef691a7fcbb65ea7dec7836f3153682f266f9400
rppatel79 <ritesh.pri.patel@gmail.com>
2016-02-07 21:24:21 -0500

    -Clean up -Refactored Originator -Starting dynomodb

--- lendingClub/src/main/java/com/rp/p2p/order_executor/OrderExecutor.java
+++ lendingClub/src/main/java/com/rp/p2p/order_executor/OrderExecutor.java
@@ -1,14 +1,36 @@
 package com.rp.p2p.order_executor;
 
-import com.rp.p2p.loan.LoanDao;
 import com.rp.p2p.model.Order;
+import com.rp.p2p.model.OrderConfirmation;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 
 
 public interface OrderExecutor {
-    LoanDao.OrderStatus  order(Collection<Order> orders)throws Exception;
+    OrderStatus  order(Collection<Order> orders)throws Exception;
 
     Map<String,Long> getPortfolios() throws Exception;
+    public static class OrderStatus
+    {
+        private final Set<OrderConfirmation> success_;
+        private final Set<OrderConfirmation> failed_;
+
+        public OrderStatus(Set<OrderConfirmation> success, Set<OrderConfirmation> failed) {
+            success_ = Collections.unmodifiableSet(success);
+            failed_ = Collections.unmodifiableSet(failed);
+        }
+
+        public Set<OrderConfirmation> getSuccess() {
+            return success_;
+        }
+
+        public Set<OrderConfirmation> getFailed() {
+            return failed_;
+        }
+    }
+
+
 }
