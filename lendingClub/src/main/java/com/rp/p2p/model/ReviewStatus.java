bc6ba2324d58158c18ebb0b36173c4be6c70b440
rppatel79 <ritesh.pri.patel@gmail.com>
2016-03-20 18:56:36 -0400

    Added states

--- lendingClub/src/main/java/com/rp/p2p/model/ReviewStatus.java
+++ lendingClub/src/main/java/com/rp/p2p/model/ReviewStatus.java
@@ -28,14 +28,26 @@ import javax.xml.bind.annotation.XmlType;
 public enum ReviewStatus {
 
         APPROVED,
-    NOT_APPROVED;
+    NOT_APPROVED,
+    CURRENT,
+    FULLY_PAID,
+    IN_GRACE_PERIOD,
+    LATE_16_30,
+    LATE_31_120,
+    DEFAULT,
+    CHARGED_OFF;
 
     public String value() {
         return name();
     }
 
     public static ReviewStatus fromValue(String v) {
-        return valueOf(v);
+        if ("LATE_(16-30_DAYS)".equals(v))
+            return LATE_31_120;
+        else if ("LATE_(31-120_DAYS)".equals(v))
+            return LATE_31_120;
+        else
+            return valueOf(v);
     }
 
 }
