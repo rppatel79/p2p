f004991a7f13d6c60e0cff32334bddb32079a4d8
rppatel79 <ritesh.pri.patel@gmail.com>
2016-04-01 20:27:25 -0400

    clean up resources

--- lendingClub/src/main/java/com/rp/p2p/backtester/Main.java
+++ lendingClub/src/main/java/com/rp/p2p/backtester/Main.java
@@ -5,6 +5,7 @@ import com.rp.p2p.loan_selector.FilteredLoansSelector;
 import com.rp.p2p.loan_selector.LoansSelector;
 import com.rp.p2p.loan_selector.RhinoLoanSelector;
 import com.rp.p2p.model.LoanListing;
+import com.rp.util.db.HibernateUtil;
 import org.apache.log4j.Logger;
 
 import java.util.Collections;
@@ -15,11 +16,16 @@ public class Main
     private final static Logger logger_ =Logger.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
-        LoanDao loanDao = new com.rp.p2p.loan.db.LoanDao();
-        List<LoanListing> loanListings = loanDao.loadAll();
+        try {
+            LoanDao loanDao = new com.rp.p2p.loan.db.LoanDao();
+            List<LoanListing> loanListings = loanDao.loadAll();
 
-        LoansSelector loansSelector = new FilteredLoansSelector(new RhinoLoanSelector(RhinoLoanSelector.SCRIPT_FILTER_LOAN_SELECTOR));
-        List<LoanListing> selected = loansSelector.select(Collections.<Long>emptySet(),loanListings);
-        logger_.info("All loans is ["+loanListings.size()+"] and selected ["+selected.size()+"]");
+            LoansSelector loansSelector = new FilteredLoansSelector(new RhinoLoanSelector(RhinoLoanSelector.SCRIPT_FILTER_LOAN_SELECTOR));
+            List<LoanListing> selected = loansSelector.select(Collections.<Long>emptySet(), loanListings);
+            logger_.info("All loans is [" + loanListings.size() + "] and selected [" + selected.size() + "]");
+        }
+        finally {
+            HibernateUtil.shutdownAll();
+        }
     }
 }
