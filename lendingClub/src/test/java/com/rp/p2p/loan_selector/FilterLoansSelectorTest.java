1b342fc61d74dc728d64cc877589570d7762f16a
Ritesh Patel <ritesh.pri.patel@gmail.com>
2017-08-05 21:01:16 -0400

    added test case

--- lendingClub/src/test/java/com/rp/p2p/loan_selector/FilterLoansSelectorTest.java
+++ lendingClub/src/test/java/com/rp/p2p/loan_selector/FilterLoansSelectorTest.java
@@ -1,7 +1,7 @@
 package com.rp.p2p.loan_selector;
 
 
-import com.rp.p2p.model.LoanListing;
+import com.rp.p2p.model.*;
 import junit.framework.TestCase;
 
 import java.util.Collections;
@@ -92,6 +92,38 @@ public class FilterLoansSelectorTest extends TestCase
 
             List<LoanListing> selected = filteredLoansSelector.select(Collections.EMPTY_SET, loans);
             TestCase.assertEquals(selected.size(),maxSize);
+        }
+
+    }
+
+    public void testEmptyTitle() throws Exception
+    {
+        int expectedSize=1;
+        FilteredLoansSelector filteredLoansSelector = new FilteredLoansSelector(new LoanSelector() {
+            @Override
+            public boolean select(Set<Long> allInvestedLoans, LoanListing ll) throws Exception {
+                return new RhinoLoanSelector(RhinoLoanSelector.SCRIPT_FILTER_LOAN_SELECTOR).select(allInvestedLoans,ll);
+            }
+        }, expectedSize);
+
+        {
+            List<LoanListing> loans = new LinkedList<LoanListing>();
+            {
+                LoanListing loanListing = new LoanListing();
+                loanListing.setIntRate(11.0);
+                loanListing.setHomeOwnership(HomeOwnership.OWN);
+                loanListing.setAnnualInc(90000);
+                loanListing.setEmpTitle(null);
+                loanListing.setPurpose(LoanPurpose.DEBT_CONSOLIDATION);
+                loanListing.setGrade(LoanGrade.A);
+                loanListing.setTerm(36);
+                loanListing.setCreditInfo(new CreditInfo());
+                loanListing.getCreditInfo().setDti(10.0);
+                loans.add(loanListing);
+            }
+
+            List<LoanListing> selected = filteredLoansSelector.select(Collections.EMPTY_SET, loans);
+            TestCase.assertEquals(selected.size(),0);
         }
 
     }
