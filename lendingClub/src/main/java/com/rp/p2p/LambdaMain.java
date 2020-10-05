929b58a3a9213e6eddca908d135a342110ac7f5c
Ritesh Patel <ritesh.pri.patel@gmail.com>
2020-10-05 12:41:25 -0700

    Support AWS Lambda

---
+++ lendingClub/src/main/java/com/rp/p2p/LambdaMain.java
@@ -0,0 +1,51 @@
+package com.rp.p2p;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.event.S3EventNotification;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class LambdaMain implements RequestHandler<Object, Object>
+{
+    static final Logger log = LoggerFactory.getLogger(LambdaMain.class);
+
+    public Object handleRequest(final Object input, final Context context)
+    {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        try {
+            String[] params = new String[]{Main.SourceType.filterSource.toString(), Double.valueOf(0.0).toString()};
+            Main.main(params);
+
+            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            return new GatewayResponse(output, headers, 200);
+        }
+        catch(Exception ex)
+        {
+            log.error("Able to complete",ex);
+
+            return new GatewayResponse("{}", headers, 500);
+        }
+    }
+
+    private String getPageContents(String address) throws IOException{
+        URL url = new URL(address);
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+}
--- lendingClub/src/main/java/com/rp/util/ApplicationProperties.java
+++ lendingClub/src/main/java/com/rp/util/ApplicationProperties.java
@@ -3,6 +3,7 @@ package com.rp.util;
 import org.apache.log4j.Logger;
 
 import java.io.*;
+import java.net.*;
 import java.util.Map;
 import java.util.Properties;
 
@@ -13,24 +14,27 @@ public class ApplicationProperties
     private static final Object LOCK = new Object();
     private static ApplicationProperties INSTANCE;
 
-    private final Logger logger_ = Logger.getLogger(ApplicationProperties.class);
-    private Properties properties_ = new Properties();
-    private String propertiesFileName_;
+    private static final Logger logger_ = Logger.getLogger(ApplicationProperties.class);
+    private final Properties properties_ = new Properties();
 
-    private ApplicationProperties() throws FileNotFoundException,IOException
+    private ApplicationProperties() throws IOException
     {
-        propertiesFileName_ = DEFAULT_PROPERTIES;
+        String propertiesFileName = DEFAULT_PROPERTIES;
         if (System.getProperty(VM_PROPERTIES) != null)
         {
-            propertiesFileName_ = System.getProperty(VM_PROPERTIES);
+            propertiesFileName = System.getProperty(VM_PROPERTIES);
         }
 
         BufferedInputStream bufferedInputStream=null;
         try {
-            File propertiesFile = new File(propertiesFileName_);
+            URL fileUrl = ApplicationProperties.class.getClassLoader().getResource(propertiesFileName);
+
+            if (fileUrl == null)
+                throw new FileNotFoundException("File does not exist. ["+ propertiesFileName +"]");
+            File propertiesFile = new File(fileUrl.getFile());
             if (!propertiesFile.exists())
                 throw new FileNotFoundException("File does not exist. ["+propertiesFile.getAbsolutePath()+"]");
-            bufferedInputStream=new BufferedInputStream(new FileInputStream(propertiesFileName_));
+            bufferedInputStream=new BufferedInputStream(new FileInputStream(propertiesFile));
             properties_.load(bufferedInputStream);
         }
         finally
